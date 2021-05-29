4f9e2eb2d6f9fc510bfefba515b2d02a1d961ce3
eunseo-kim <67737432+eunseo-kim@users.noreply.github.com>
2021-05-29 21:02:20 +0900

    쪽지 보내기, 쪽지함(수신함, 송신함) 완성
    - 아직 쪽지 내용 보기 기능은 미완성
    - 쪽지함 css에서 상단 버튼 리스트 디자인이 마음에 들지는 않지만 일단...
    - 화면 크기 작아지면 쪽지함의 상단 버튼 리스트가 수직 정렬됨

---
+++ message_box_screen.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Daily</title>
+    <!-- favicon 사용 -->
+    <link rel="icon" href="./img/favicon.png"/>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Pattaya&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet" />
+    <!-- 기본 font : Gothic A1 -->
+    <link href="https://fonts.googleapis.com/css2?family=Gothic+A1:wght@300&display=swap" rel="stylesheet" />
+
+    <script src="https://kit.fontawesome.com/f1def33959.js" crossorigin="anonymous"></script>
+    <!-- reset css -->
+    <link rel="stylesheet" href="./css/reset.css" />
+    <!-- 메뉴바 css-->
+    <link rel="stylesheet" href="./css/navbar.css" />
+    <!-- 쪽지함 css -->
+    <link rel="stylesheet" href="./css/message_box.css" />
+
+    <script src="./js/main.js" defer></script>
+  </head>
+  <body>
+    <!-- header.php 실행 -->
+    <header>
+      <?php include "./php/header.php";?>
+    </header>
+
+    <section>
+      <div id="message_box">
+        <ul class="buttons">
+          <li><button onclick="location.href='message_box_screen.php?mode=rv'">받은 쪽지함</button></li>
+          <li><button onclick="location.href='message_box_screen.php?mode=send'">보낸 쪽지함</button></li>
+          <li><button onclick="location.href='message_screen.php'">쪽지 보내기</button></li>
+        </ul>
+        <h3>
+          <?php
+              if (isset($_GET["page"]))
+                $page = $_GET["page"];
+              else
+                $page = 1;
+
+              $mode = $_GET["mode"];
+
+              if ($mode=="send")
+                echo "📩 보낸 쪽지함";
+              else
+                echo "📨 받은 쪽지함";
+          ?>
+        </h3>
+        <div>
+          <ul id="message">
+            <li>
+              <span class="col1">번호</span>
+              <span class="col2">제목</span>
+              <span class="col3">
+              <?php						
+                // 내가 '누구'에게 보내는지 
+                if ($mode=="send")
+                  echo "받는 사람";
+                else
+                // '누가' 내게 보냈는지
+                  echo "보낸 사람";
+              ?>
+              </span>
+              <span class="col4">날짜</span>
+            </li>
+            <?php
+              $con = mysqli_connect("localhost", "eunseo", "1205", "diary");
+
+              if ($mode=="send")
+                $sql = "select * from message where send_id='$userid' order by num desc";
+              else
+                $sql = "select * from message where rv_id='$userid' order by num desc";
+
+              $result = mysqli_query($con, $sql);
+              $total_record = mysqli_num_rows($result); // 전체 글 수
+
+              $scale = 10;
+
+              // 전체 페이지 수($total_page) 계산 
+              if ($total_record % $scale == 0)     
+                $total_page = floor($total_record/$scale);      
+              else
+                $total_page = floor($total_record/$scale) + 1; 
+            
+              // 표시할 페이지($page)에 따라 $start 계산  
+              $start = ($page - 1) * $scale;      
+
+              $number = $total_record - $start;
+
+            for ($i=$start; $i<$start+$scale && $i < $total_record; $i++)
+            {
+              mysqli_data_seek($result, $i);
+              // 가져올 레코드로 위치(포인터) 이동
+              $row = mysqli_fetch_array($result);
+              // 하나의 레코드 가져오기
+              $num    = $row["num"];
+              $subject     = $row["subject"];
+              $regist_day  = $row["regist_day"];
+
+              if ($mode=="send")
+                $msg_id = $row["rv_id"];
+              else
+                $msg_id = $row["send_id"];
+              
+              $result2 = mysqli_query($con, "select name from members where id='$msg_id'");
+              $record = mysqli_fetch_array($result2);
+              $msg_name     = $record["name"];	  
+            ?>
+            <li>
+              <span class="col1"><?=$number?></span>
+              <span class="col2"><a href="message_view.php?mode=<?=$mode?>&num=<?=$num?>"><?=$subject?></a></span>
+              <span class="col3"><?=$msg_name?>(<?=$msg_id?>)</span>
+              <span class="col4"><?=$regist_day?></span>
+            </li>	
+            <?php
+              $number--;
+            }
+            mysqli_close($con);
+            ?>
+          </ul>
+        <ul id="page_num"> 	
+          <?php
+            if ($total_page>=2 && $page >= 2)	
+            {
+              $new_page = $page-1;
+              echo "<li><a href='message_box_screen.php?mode=$mode&page=$new_page'>◀ 이전</a> </li>";
+            }		
+            else 
+              echo "<li>&nbsp;</li>";
+
+            // 게시판 목록 하단에 페이지 링크 번호 출력
+            for ($i=1; $i<=$total_page; $i++)
+            {
+              if ($page == $i)     // 현재 페이지 번호 링크 안함
+              {
+                echo "<li><b> $i </b></li>";
+              }
+              else
+              {
+                echo "<li> <a href='message_box_screen.php?mode=$mode&page=$i'> $i </a> <li>";
+              }
+            }
+            if ($total_page>=2 && $page != $total_page)		
+            {
+              $new_page = $page+1;	
+              echo "<li> <a href='message_box_screen.php?mode=$mode&page=$new_page'>다음 ▶</a> </li>";
+            }
+            else 
+              echo "<li>&nbsp;</li>";
+          ?>
+        </ul> <!-- page -->	    	
+      </div> <!-- message_box -->
+    </section> 
+  </body>
+</html>
--- message_screen.php
+++ message_screen.php
@@ -35,7 +35,57 @@
       <?php include "./php/header.php";?>
     </header>
     <section>
-        <?php include "./php/message_form.php"; ?>
+      <?php
+        if (!$userid )
+        {
+          echo("<script>
+              alert('로그인 후 이용해주세요!');
+              history.go(-1);
+              </script>
+            ");
+          exit;
+        }
+      ?>
+      <div id="message_box">
+          <div class="login_title__icon"><i class="fab fa-ethereum"></i></div>
+          <h3 id="write_title">
+                  Send a Message
+          </h3>
+          <!-- 실제로 입력 하는 부분 -->
+          <div id="message_box__input_box">
+              <ul class="top_buttons">
+                  <li><span><a href="message_box_screen.php?mode=rv" class="top_buttons__rv">받은 쪽지함 </a></span></li>
+                  <li><span><a href="message_box_screen.php?mode=send" class="top_buttons__send">보낸 쪽지함</a></span></li>
+              </ul>
+              <!-- 입력하면 message_insert.php에 전송 -->
+              <form  name="message_form" method="post" action="./php/message_insert.php?send_id=<?=$userid?>">
+                  <div id="write_msg">
+                      <ul>
+                      <li>
+                          <span class="col1">보내는 사람</span>
+                          <span class="col2"><?=$userid?></span>
+                      </li>	
+                      <li>
+                          <span class="col1">수신 아이디</span>
+                          <span class="col2"><input name="rv_id" type="text"></span>
+                      </li>	
+                      <li>
+                          <span class="col1">제목</span>
+                          <span class="col2"><input name="subject" type="text"></span>
+                      </li>	    	
+                      <li id="text_area">	
+                          <span class="col1">내용</span>
+                          <span class="col2">
+                              <textarea name="content" style = "resize: none"></textarea>
+                          </span>
+                      </li>
+                      </ul>
+                  </div>	    	
+              </form>
+              
+          </div>
+          <button type="button" onclick="check_input()">보내기</button>
+      </div> <!-- message_box -->
     </section>
   </body>
 </html>
