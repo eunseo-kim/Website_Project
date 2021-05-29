e6f5cbf65ed9cfbeb43006bab6af2c5dd46e338d
eunseo-kim <67737432+eunseo-kim@users.noreply.github.com>
2021-05-30 04:18:41 +0900

    Gallery 게시판 html, css, js 완성
    - 나머지 기능 구현(php 부분)은 미완성
    - velog 디자인을 참고하여 만듦
    - 하단부(목록보기, 출간하기)를 position fixed로 배치했음.
    - 단, button의 색깔이 조금 마음에 안듦...

---
+++ board_form_screen.php
@@ -0,0 +1,69 @@
+<!-- 글쓰기 폼 화면 -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Daily</title>
+    <!-- favicon 사용 -->
+    <link rel="icon" href="./img/favicon.png"/>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Pattaya&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet" />
+    <!-- 기본 font : Gothic A1 -->
+    <link href="https://fonts.googleapis.com/css2?family=Gothic+A1:wght@300&display=swap" rel="stylesheet" />
+
+    <script src="https://kit.fontawesome.com/f1def33959.js" crossorigin="anonymous"></script>
+    <script>
+      function check_input() {
+        if (!document.board_form.subject.value)
+        {
+            alert("제목을 입력하세요!");
+            document.board_form.subject.focus();
+            return;
+        }
+        if (!document.board_form.content.value)
+        {
+            alert("내용을 입력하세요!");    
+            document.board_form.content.focus();
+            return;
+        }
+        document.board_form.submit();
+      }
+    </script>
+
+    <!-- reset css -->
+    <link rel="stylesheet" href="./css/reset.css" />
+    <!-- 메뉴바 css-->
+    <link rel="stylesheet" href="./css/navbar.css" />
+    <!-- 글쓰기 css -->
+    <link rel="stylesheet" href="./css/board_form.css" />
+
+    <script src="./js/main.js" defer></script>
+  </head>
+  <body>
+    <!-- header.php 실행 -->
+    <header>
+      <?php include "./php/header.php";?>
+    </header>
+    <section>
+   	<div id="board_box">
+	    <form  name="board_form" method="post" action="./php/board_insert.php" enctype="multipart/form-data">
+	    	 <ul id="board_form">	
+	    			<span class="board_form__title"><input name="subject" type="text" placeholder ="제목을 입력하세요"></span>
+            <span class="board_form__image_file"><input type="file" name="upfile"></span>
+            <textarea name="content" placeholder="당신의 이야기를 적어보세요..."></textarea>
+          </ul>
+          <ul class="buttons">
+                <li><button type="button" onclick="check_input()">출간하기</button></li>
+                <li><button type="button" onclick="location.href='board_list.php'">목록보기</button></li>
+              </ul>
+	    </form>
+	</div> <!-- board_box -->
+</section> 
+  </body>
+</html>
--- php/header.php
+++ php/header.php
@@ -14,7 +14,8 @@
       </div>
       <ul class="navbar__menu">
         <li><a href="main_screen.php">Home</a></li>
-        <li><a href="#">Gallary</a></li>
+        <!-- 나중에 board_list_screen.php로 바꿀 예정 -->
+        <li><a href="board_form_screen.php">Gallery</a></li>
         <li><a href="#">Schedule</a></li>
         <li><a href="#">Diary</a></li>
         <li><a href="./message_screen.php">Message</a></li>
