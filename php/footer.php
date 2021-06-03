db4450532d233e5232e51cb2b1c8fc2801494ac5
eunseo-kim <67737432+eunseo-kim@users.noreply.github.com>
2021-06-03 21:06:25 +0900

    Update footer.php

--- php/footer.php
+++ php/footer.php
@@ -4,6 +4,6 @@
             <span><a href="https://github.com/eunseo-kim" target='_blank'><i class="fab fa-github-alt"></i></a></span>
             <span><a href="https://velog.io/@eunseokim" target='_blank'><i class="fab fa-vimeo-square"></i></a></span>
         </div>
-        <div class="copyright">Copyright 2021. Diary All Rights Reserved.</div>
+        <div class="copyright">Copyright 2021. Daily All Rights Reserved.</div>
     </div>
 </footer>
