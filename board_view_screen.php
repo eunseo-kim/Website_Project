d73d112d986858bd977b1f561044eb366042b660
eunseo-kim <67737432+eunseo-kim@users.noreply.github.com>
2021-06-03 19:22:44 +0900

    자신의 글만 수정, 삭제 가능

--- board_view_screen.php
+++ board_view_screen.php
@@ -138,9 +138,13 @@
         </div>
         <div class="button_right">
           <button onclick="location.href='board_form_screen.php'">글쓰기</button>
-          <button onclick="location.href='board_modify_form_screen.php?num=<?=$num?>&page=<?=$page?>'">수정</button>
-          <button onclick="location.href='./php/board_delete.php?num=<?=$num?>&page=<?=$page?>'">삭제</button>
-        </div>
+          <!-- 수정(자기자신의 글만 수정, 삭제 가능) -->
+          <?php
+            if ($userid == $id) {
+            echo "<button onclick='location.href='board_modify_form_screen.php?num=<?=$num?>&page=<?=$page?>''>수정</button>";
+            echo "<button onclick='location.href='./php/board_delete.php?num=<?=$num?>&page=<?=$page?>''>삭제</button>";
+          }?>
+          </div>
 		</ul>
 	</div> <!-- board_box -->
 </section> 
