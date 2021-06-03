d1f47fdc9c01dd020211e42fad2ef14620fae592
eunseo-kim <67737432+eunseo-kim@users.noreply.github.com>
2021-06-03 21:32:09 +0900

    Update sign_up_screen.php

--- sign_up_screen.php
+++ sign_up_screen.php
@@ -19,11 +19,71 @@
     <link rel="stylesheet" href="./css/reset.css" />
     <!-- 메뉴바 css-->
     <link rel="stylesheet" href="./css/navbar.css" />
-<link rel="stylesheet" href="./css/footer.css" />
+    <link rel="stylesheet" href="./css/footer.css" />
     <!-- 회원가입 css -->
     <link rel="stylesheet" href="./css/sign_up.css" />
+    <script>
+      function check_id() {
+          window.open("./php/member_check_id.php?id=" + document.member_form.id.value, "IDcheck", "left=700,top=300,width=350,height=200,scrollbars=no,resizable=no");
+        //    .open("/php/member_check_id.php?id=" + document.member_form.id.value, "IDcheck", "left=700,top=300,width=350,height=200,scrollbars=no,resizable=no");
+        }
+
+        function check_input() {
+          if (!document.member_form.pass.value) {
+            alert("비밀번호를 입력하세요!");
+            document.member_form.pass.focus();
+            return;
+          }
+
+          if (!document.member_form.pass_confirm.value) {
+            alert("비밀번호확인을 입력하세요!");
+            document.member_form.pass_confirm.focus();
+            return;
+          }
+
+          if (!document.member_form.name.value) {
+            alert("이름을 입력하세요!");
+            document.member_form.name.focus();
+            return;
+          }
+
+          if (!document.member_form.email1.value) {
+            alert("이메일 주소를 입력하세요!");
+            document.member_form.email1.focus();
+            return;
+          }
+
+          if (!document.member_form.email2.value) {
+            alert("이메일 주소를 입력하세요!");
+            document.member_form.email2.focus();
+            return;
+          }
+
+          if (document.member_form.pass.value != document.member_form.pass_confirm.value) {
+            alert("비밀번호가 일치하지 않습니다.\n다시 입력해 주세요!");
+            document.member_form.pass.focus();
+            document.member_form.pass.select();
+            return;
+          }
+
+          document.member_form.submit();
+        }
+
+        function reset_form() {
+          document.member_form.id.value = "";
+          document.member_form.pass.value = "";
+          document.member_form.pass_confirm.value = "";
+          document.member_form.name.value = "";
+          document.member_form.email1.value = "";
+          document.member_form.email2.value = "";
+
+          document.member_form.id.focus();
+
+          return;
+        }
+
+    </script>
     <script src="./js/main.js" defer></script>
-    <script src="./js/sign_up.js" defer></script>
   </head>
   <body>
     <!-- header.php 실행 -->
